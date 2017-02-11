755a62b42bf018e689c4dfe49bf9df532697da12
Altair <maciejkamat@gmail.com>
2017-02-11 18:38:04 +0100

    tiny linux build change
    turns out it works on bash on ubuntu on windows 10 (through x11 server
    on windows, ofc)

---
+++ build_linux.sh
@@ -0,0 +1,2 @@
+gcc src/main.c src/main_menu.c src/load_dialog.c src/gameplay.c src/utils.c src/engine.c src/parser.c -std=c99 -Wall -Wextra -Wshadow -Wconversion -lSDL2 -lSDL2_net -lm -lGL -lGLU -o out/g7.out
+
